7b83a81448879911643db8714f0084904af2cea1
rimi <a.fatimaarimi@gmail.com>
2023-07-19 14:23:17 -0400

    adding pseudocode

---
+++ script.js
@@ -0,0 +1,28 @@
+/**
+ * * Game: Flip It
+ * 
+ * Player has the option of choosing easy, medium, and hard -> 
+ * Based on the mode player has chosen - a certain amount of cards will display:
+ * * Easy - 2x2 = 4
+ * * Medium - 4x4 = 16
+ * * Hard - 6x6 = 36
+ * ! (optional) Challenging - 8x8 = 64
+ * 
+ * 
+ * All colors will be randomized and shuffled for every round:
+ * Max card amount = 64
+ * Total color amount = 32 (color pairs)
+ * Add an array of colors
+ * Access the array and apply it to startGame
+ * Add a shuffle function 
+ * 
+ * select all memory cards
+ * flip card function ->
+ * * when you click a card
+ * * it will toggle from front-face to back-face
+ * * 
+ * * 
+ * 
+ * 
+ * */
+
