21eabb7745004d77d49f7db41809c8710b33e816
rimi <a.fatimaarimi@gmail.com>
2023-07-19 16:38:15 -0400

    some more notes

--- script.js
+++ script.js
@@ -56,27 +56,45 @@ hardMode.addEventListener("click", () => {
   createGameBoard(4, 4);
 });
 
-// create gameboard function -- that changes based off of game mode (rearranges rows and columns)
+// select the .container class which is a descendent of .gameboard
 const gameboard = document.querySelector(".gameboard .container");
+
+// create a createGameBoard function with the parameters of row, and column which will determine the number of rows and columns
 function createGameBoard(row, column) {
   // Clear the current game board
   gameboard.innerHTML = "";
 
-  // Gotta figure out a way to make an array of colors and shuffle them around for every new game
-
   // Calculate the total number of cards
   const totalCards = row * column;
+  
+  // Gotta figure out a way to make an array of colors and shuffle them around for every new game
 
-  // Setting up the game board grid layout -- responsible for rearranging and adjusting the grid layout of the game based off of chosen mode
+  // * Setting up the game board grid layout -- responsible for rearranging and adjusting the grid layout of the game based off of chosen mode
   // This is basically saying gameboard will have a grid style for both row and column
   // Depending on the mode player chose the card will repeat until it meets that number amount
   // 1fr means it will take up an equal fraction of that amount of space so for example if the column and row is meant to be 4x4, each card will take up 1/4 of the row and column space
   gameboard.style.gridTemplateColumns = `repeat(${column}, 1fr)`;
   gameboard.style.gridTemplateRows = `repeat(${row}, 1fr)`;
 
-  // Create a for loop that creates new and inserts new divs based off the mode that was chosen
+  // * Create a for loop that creates new and inserts new divs based off the mode that was chosen
   // should add a memory card
   // with a front-face and back-face
   // Append the front-face and back-face to the memory-card
   // append the actual card to the gameboard
 }
+
+// * a function to get random colors
+
+// * a function to shuffle the colors
+
+// * 
+
+
+/**
+ * 
+ * CREDITS & RESOURCES:
+ * - https://www.geeksforgeeks.org/javascript-generate-random-hex-codes-color/#
+ * - 
+ * 
+ * 
+ */
